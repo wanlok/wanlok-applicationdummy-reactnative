c4b94262733a6c4ce88f01c9b1f3f10203fc7b43
Robert Wan <wantunglok@gmail.com>
2025-02-16 15:11:48 +1100

    rename bundle

--- android/app/src/main/java/com/wanlok/applicationdummy/MainApplication.java
+++ android/app/src/main/java/com/wanlok/applicationdummy/MainApplication.java
@@ -1,4 +1,4 @@
-package com.dummy;
+package com.wanlok.applicationdummy;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/applicationdummyTests/applicationdummyTests.m
+++ ios/applicationdummyTests/applicationdummyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface dummyTests : XCTestCase
+@interface applicationdummyTests : XCTestCase
 
 @end
 
-@implementation dummyTests
+@implementation applicationdummyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
