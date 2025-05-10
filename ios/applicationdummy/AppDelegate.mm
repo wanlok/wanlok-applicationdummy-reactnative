06d6abcf50591ce24185407814a205f65c8262d8
Robert Wan <wantunglok@gmail.com>
2025-05-10 18:57:42 +1000

    add firebase push notification support

--- ios/applicationdummy/AppDelegate.mm
+++ ios/applicationdummy/AppDelegate.mm
@@ -2,18 +2,18 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
-
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"applicationdummy";
   self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './Pages/Home'
 import Application from './Pages/Application'
+import messaging from '@react-native-firebase/messaging'
+import { useEffect } from 'react'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -11,6 +13,50 @@ export type RootStackParamList = {
 }
 
 const App = () => {
+  useEffect(() => {
+    // Request permission on iOS
+    messaging().requestPermission()
+    // Listen for foreground messages
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived: ' + JSON.stringify(remoteMessage))
+    })
+    return unsubscribe
+  }, [])
+
+  useEffect(() => {
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log('Notification caused app to open from background/killed:', remoteMessage)
+          // You can navigate or handle the message here
+        }
+      })
+  }, [])
+
+  useEffect(() => {
+    messaging()
+      .getToken()
+      .then(token => {
+        console.log('FCM Token:', token)
+      })
+  }, [])
+
+  // useEffect(() => {
+  //   messaging()
+  //     .requestPermission()
+  //     .then(authStatus => {
+  //       if (
+  //         authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+  //         authStatus === messaging.AuthorizationStatus.PROVISIONAL
+  //       ) {
+  //         console.log('Notification permission granted.')
+  //       } else {
+  //         console.log('Notification permission denied.')
+  //       }
+  //     })
+  // }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
