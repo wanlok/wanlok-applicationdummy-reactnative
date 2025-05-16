31d9aa51811a4262e4829d58485fe1407a1129a4
Robert Wan <wantunglok@gmail.com>
2025-05-16 20:49:42 +1000

    update menu page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -5,6 +5,7 @@ import DPage from '../../Components/DPage'
 import DButton from '../../Components/DButton'
 import { usePushNotification } from '../../Components/PushNotification/PushNotificationContext'
 import { useAuth0 } from 'react-native-auth0'
+import { FlatList, Text } from 'react-native'
 
 export default ({ navigation }: { navigation: NativeStackNavigationProp<RootStackParamList> }) => {
   const { pushNotification, show, setShow } = usePushNotification()
@@ -15,51 +16,55 @@ export default ({ navigation }: { navigation: NativeStackNavigationProp<RootStac
     console.log(pushNotification)
   }, [pushNotification])
 
+  const data = [
+    {
+      title: 'Application',
+      onClick: () => navigation.navigate('Application'),
+    },
+    {
+      title: 'Loan List',
+      onClick: () => navigation.navigate('LoanList'),
+    },
+    {
+      title: 'Login',
+      onClick: async () => {
+        try {
+          await authorize()
+        } catch (e) {
+          console.log(e)
+        }
+      },
+    },
+    {
+      title: 'Logout',
+      onClick: async () => {
+        try {
+          await clearSession()
+        } catch (e) {
+          console.log(e)
+        }
+      },
+    },
+    {
+      title: 'Test Bottom Sheet',
+      onClick: () => {
+        setShow(!show)
+      },
+    },
+  ]
+
   return (
-    <>
-      <DPage>
-        <DButton onClick={() => navigation.navigate('Application')}>Application</DButton>
-        <DButton
-          style={{ marginTop: 16 }}
-          onClick={() => {
-            setShow(!show)
-          }}
-        >
-          Test Bottom Sheet
-        </DButton>
-        <DButton
-          style={{ marginTop: 16 }}
-          onClick={() => {
-            navigation.navigate('LoanList')
-          }}
-        >
-          Loan List
-        </DButton>
-        <DButton
-          style={{ marginTop: 16 }}
-          onClick={async () => {
-            try {
-              await authorize()
-            } catch (e) {
-              console.log(e)
-            }
-          }}
-        >
-          Login
-        </DButton>
-        <DButton
-          style={{ marginTop: 16 }}
-          onClick={async () => {
-            try {
-              await clearSession()
-            } catch (e) {
-              console.log(e)
-            }
-          }}
-        >
-          Logout
-        </DButton>
-      </DPage>
-    </>
+    <DPage>
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          return (
+            <DButton style={{ marginTop: index > 0 ? 1 : 0 }} onClick={item.onClick}>
+              {item.title}
+            </DButton>
+          )
+        }}
+      />
+    </DPage>
   )
 }
