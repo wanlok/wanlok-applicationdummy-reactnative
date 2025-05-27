60ca485321ed0ff548018e8fabfbc5b86d89279e
Robert Wan <wantunglok@gmail.com>
2025-05-28 00:20:11 +1000

    ask push notification permission

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
@@ -8,8 +8,29 @@ import { PushNotificationProvider } from './Hooks/PushNotificationContext'
 import Navigation from './Components/Navigation'
 import { Auth0Provider } from 'react-native-auth0'
 import Config from 'react-native-config'
+import { PermissionsAndroid, Platform } from 'react-native'
 
 const App = () => {
+  const requestPermission = async () => {
+    const title = 'Push Notification Permission'
+    const message = 'Allow push notifications to receive important updates about your account.'
+    const buttonPositive = 'OK'
+    const { OS, Version } = Platform
+    let status = null
+    if (OS === 'android' && Version >= 33) {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        { title, message, buttonPositive },
+      )
+      status = granted === PermissionsAndroid.RESULTS.GRANTED
+    }
+    console.log(title, OS, Version, status)
+  }
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
+
   return (
     <Auth0Provider domain={Config.AUTH0_DOMAIN ?? ''} clientId={Config.AUTH0_CLIENT_ID ?? ''}>
       <AuthenticationProvider>
